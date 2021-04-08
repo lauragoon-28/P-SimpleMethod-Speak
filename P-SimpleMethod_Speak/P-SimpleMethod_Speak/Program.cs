ada4ef696999a2f5e7a3d4fcc27dd8b9f8f3a6b3
Laura Goon <Laura.goon@ou.edu>
2021-04-08 18:36:35 -0500

    Almost finished the code

--- P-SimpleMethod_Speak/P-SimpleMethod_Speak/Program.cs
+++ P-SimpleMethod_Speak/P-SimpleMethod_Speak/Program.cs
@@ -7,20 +7,45 @@ namespace P_SimpleMethod_Speak
         static void Main(string[] args)
         {
             string answer;
-            Console.WriteLine("Please enter a type of animal");
-            answer = Console.ReadLine();
+            Console.WriteLine("Please enter a type of animal.(dog, cat or lion)");
+            answer = Console.ReadLine().ToLower();
+            string animalSound = Speak(answer);
 
+            if (answer == "dog", "cat", "lion")
+            {
+                Console.WriteLine($"A {answer} makes a {animalSound} sound! Yay!");
+            }
 
-
+            else
+            {
+                Console.WriteLine("Sorry that animal wasn't found. Goodbye!");
+            }
 
         }
-
-        static string Speak(string sound)
+        /// <summary>
+        /// Converts the animal into the sound that the animal they make
+        /// </summary>
+        /// <param name="animal">A string representing an animal</param>
+        /// <returns>The sound the animal makes, if the animal isn't found the code will exit.</returns>
+        static string Speak(string animal)
         {
-            string
+            string sound = "";
+
+            if (animal == "dog")
+            {
+                sound = "woof";
+            }
+            else if (animal == "cat")
+            {
+                sound = "meow";
+            }
+            else if (animal == "lion")
+            {
+                sound = "rawr";
+            }
+            return sound;
 
 
         }
-    }
     }
 }
