0e97ee802f1a9e19333ec127ab218a7d49d99563
Laura Goon <Laura.goon@ou.edu>
2021-04-08 18:01:44 -0500

    created solution and coded some stuff

---
+++ P-SimpleMethod_Speak/P-SimpleMethod_Speak/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P_SimpleMethod_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            Console.WriteLine("Please enter a type of animal");
+            answer = Console.ReadLine();
+
+
+
+
+        }
+
+        static string Speak(string sound)
+        {
+            string
+
+
+        }
+    }
+    }
+}
